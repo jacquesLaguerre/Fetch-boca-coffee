cb1e4c73492df5debca59df132400ebd660c3b59
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-26 15:33:35 -0400

    Changes Heading, Gives Time but no location yet

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-const coffeeContainer = document.querySelector('main')
+const coffeeContainer = document.querySelector("main");
 
 function setLoading() {
-    coffeeContainer.innerHTML =''
- const h2 = document.createElement('h2')
- const loading = document.createTextNode('LOADING...')
- h2.appendChild(loading)
- coffeeContainer.appendChild(h2)
-
+  coffeeContainer.innerHTML = "";
+  const h2 = document.createElement("h2");
+  const loading = document.createTextNode("LOADING...");
+  h2.appendChild(loading);
+  coffeeContainer.appendChild(h2);
 }
 
-function displayCoffeeList(coffeeList){
-    coffeeContainer.innerHTML = ''
-   coffeeList.map(coffee => {
- const card = document.createElement('div')
- const img = document.createElement('img')
- const title = document.createElement('h3')
- const description = document.createElement('p')
- title.appendChild(document.createTextNode(coffee.title))
- description.appendChild(document.createTextNode(coffee.description))
- card.appendChild(title)
- card.appendChild(description)
- coffeeContainer.appendChild(card)
-    })
+function displayCoffeeList(coffeeList) {
+  coffeeContainer.innerHTML = "";
+  coffeeList.map((coffee) => {
+    const card = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h3");
+    const description = document.createElement("p");
+    title.appendChild(document.createTextNode(coffee.title));
+    description.appendChild(document.createTextNode(coffee.description));
+    card.appendChild(title);
+    card.appendChild(description);
+    coffeeContainer.appendChild(card);
+  });
 }
 
-function getCoffee() {
-    setLoading()
-    fetch(`https://api.sampleapis.com/coffee/hot`)
-    .then(response => response.json())
+function getCoffee(temp) {
+  setLoading();
+  fetch(`https://api.sampleapis.com/coffee/${temp}`)
+    .then((response) => response.json())
     .then(displayCoffeeList)
-    .catch(console.error) // TODO: display error for user 
+    .catch(console.error); // TODO: display error for user
 }
 
-
-
-getCoffee()
+getCoffee('hot');
